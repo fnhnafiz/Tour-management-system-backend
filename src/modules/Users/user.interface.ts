b5b5be5dbc5d123bc9602684b15bfaabf6494fbb
fnhnafiz <nafsinsifan2k19@gmail.com>
2026-01-30 19:37:11 +0600

    create user and work in user folder

---
+++ src/modules/Users/user.interface.ts
@@ -0,0 +1,35 @@
+import { Types } from "mongoose";
+
+export enum ROLE {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  USER = "USER",
+  GUIDE = "GUIDE",
+}
+
+export interface IAuthUser {
+  provider: string;
+  providerId: string;
+}
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  picture?: string;
+  address?: string;
+  isDeleted?: string;
+  isActive?: IsActive;
+  isVerified?: string;
+  role: ROLE;
+  auths: IAuthUser[];
+  bookings?: Types.ObjectId[];
+  guides?: Types.ObjectId[];
+}
