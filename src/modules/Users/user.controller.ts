6ed7ca6437f9e54edd9c186972fea42f6dd06741
fnhnafiz <nafsinsifan2k19@gmail.com>
2026-02-08 08:08:59 -0600

    global error

--- src/modules/Users/user.controller.ts
+++ src/modules/Users/user.controller.ts
@@ -1,24 +1,27 @@
-import { Request, Response } from "express";
-import { User } from "./user.model";
+import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
+import { UserServices } from "./user.service";
 
-const createUser = async (req: Request, res: Response) => {
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { email, name } = req.body;
-    const user = await User.create({
-      name,
-      email,
-    });
+    // const { email, name } = req.body;
+    // const user = await User.create({
+    //   name,
+    //   email,
+    // });
+    // throw new Error("Fake Error for testing global error handler!!");
+    const user = await UserServices.createUser(req.body);
     res.status(httpStatus.CREATED).json({
-      user,
       message: "User created succesfully",
+      user,
     });
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     // console.log("Somthing wrong!!");
-    res.status(httpStatus.BAD_REQUEST).json({
-      message: `Something wen wrong ${error.message}`,
-    });
+    // res.status(httpStatus.BAD_REQUEST).json({
+    //   message: `Something wen wrong ${error.message}`,
+    // });
+    next(error);
   }
 };
 
